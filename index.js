d2722cd557489496594a2f90afa062fba6820b1c
Aditya Agarwal <adityaa803@gmail.com>
2018-03-17 14:25:44 +0530

    fixes #1

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 module.exports = robot => {
   robot.log('Started')
   robot.on('issues.opened', async context => {
+    const ISSUE_AUTHOR = context.payload.issue.user.login
     context.log('issue is opened')
-    const AUTHOR = context.repo().owner;
+    const REPO_OWNER = context.repo().owner
+    const isOwner = context.payload.issue.author_association === 'OWNER'
+    if(isOwner) {
+      return;
+    }
+    context.log('issue opener is not the owner')
     const params = context.issue({
-      body: `Hey There! Thank you so much for taking out the time to open an issue in this repo.
-      @${AUTHOR} will get back to you ASAP.`
+      body: `Hi @${ISSUE_AUTHOR}! Thank you so much for taking out the time to open an issue in this repo.
+      @${REPO_OWNER} will get back to you ASAP.`
     })
     return context.github.issues.createComment(params)
   })
