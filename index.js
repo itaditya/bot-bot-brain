b4839fbeee10330489be6caa643678ad9d9bb01e
Aditya Agarwal <adityaa803@gmail.com>
2018-03-26 00:19:45 +0530

    hour added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = robot => {
   robot.log('Started')
 
   const { hourScheduler } = require('./utils/schedule-setup')(robot)
-  // hourScheduler()
+  hourScheduler()
   require('./routes')(robot)
   require('./utils/github-replier')(robot)
   require('./utils/reminder')(robot)
